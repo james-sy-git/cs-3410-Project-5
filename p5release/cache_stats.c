0904204ebf20d28e2e70682cd330ff9bc5fc872a
Rowan Anikath Hennessy <rah379@en-ci-cisugcl20..coecis.cornell.edu>
2022-11-16 22:50:19 -0500

    Task 9

--- p5release/cache_stats.c
+++ p5release/cache_stats.c
@@ -38,7 +38,7 @@ cache_stats_t *make_cache_stats() {
  * to correctly update on LD_MISS and ST_MISS actions
  * also need to update total_snoop_hits, total_bus_snoops
 */
-void update_stats(cache_stats_t *stats, bool hit_f, bool writeback_f, bool upgrade_miss_f, enum action_t action) {
+void update_stats(cache_stats_t *stats, bool hit_f, bool writeback_f, bool upgrade_miss_f, bool bus_snoop, bool snoop_hit, enum action_t action) {
   if (hit_f)
     stats->n_hits++;
   
@@ -51,6 +51,11 @@ void update_stats(cache_stats_t *stats, bool hit_f, bool writeback_f, bool upgra
   if (upgrade_miss_f)
     stats->n_upgrade_miss++;
 
+  if (bus_snoop)
+    stats->n_bus_snoops++;
+
+  if(snoop_hit)
+    stats->n_snoop_hits++; 
   stats->n_cpu_accesses++;
 }
 
